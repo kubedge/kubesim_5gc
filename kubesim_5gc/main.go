2c83b32ab8ff7485d65922b68ba6a494c113cbbb
Jerome Brette <jbrette@gmail.com>
2018-11-10 17:17:02 +0000

    Run gofmt on GO code
    Change-Id: Ia0b41a2a9ff5536a31a020372d2ecd0c7d500644

--- kubesim_5gc/main.go
+++ kubesim_5gc/main.go
@@ -15,37 +15,38 @@ limitations under the License.
 */
 
 package main
+
 import (
-  "log"
-  "net/http"
-  "strings"
-  "github.com/kubedge/kubesim_base/grpc/go/kubedge_server"
-  "github.com/kubedge/kubesim_base/config"
-  "github.com/kubedge/kubesim_base/connected"
+	"github.com/kubedge/kubesim_base/config"
+	"github.com/kubedge/kubesim_base/connected"
+	"github.com/kubedge/kubesim_base/grpc/go/kubedge_server"
+	"log"
+	"net/http"
+	"strings"
 )
+
 func sayHello(w http.ResponseWriter, r *http.Request) {
-  message := r.URL.Path
-  message = strings.TrimPrefix(message, "/")
-  message = "kubesim 5gc simulator " + message
-  w.Write([]byte(message))
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "kubesim 5gc simulator " + message
+	w.Write([]byte(message))
 }
 func main() {
-  log.Printf("%s", "kubesim_5gc is running")
+	log.Printf("%s", "kubesim_5gc is running")
 
+	var conf config.Configdata
+	conf.Config()
+	log.Printf("5gc server:  product_name=%s, product_type=%s, product_family=%s, product_release=%s, feature_set1=%s, feature_set2=%s",
+		conf.Product_name, conf.Product_type, conf.Product_family, conf.Product_release, conf.Feature_set1, conf.Feature_set2)
 
-  var conf config.Configdata
-  conf.Config()
-  log.Printf("5gc server:  product_name=%s, product_type=%s, product_family=%s, product_release=%s, feature_set1=%s, feature_set2=%s",
-             conf.Product_name, conf.Product_type, conf.Product_family, conf.Product_release, conf.Feature_set1, conf.Feature_set2)
+	var conn connected.Connecteddata
+	conn.Readconnectvalues()
+	log.Printf("5gc server:  connected=%s", conn.Connected)
 
-  var conn connected.Connecteddata
-  conn.Readconnectvalues()
-  log.Printf("5gc server:  connected=%s", conn.Connected)
-
-  //run server forever
-  server.Server(conf)
-  http.HandleFunc("/", sayHello)
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    panic(err)
-  }
+	//run server forever
+	server.Server(conf)
+	http.HandleFunc("/", sayHello)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
